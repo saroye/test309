c005c328396b0680ab6d9459d85d093eaf689da9
mohit <saroyemohit@gmail.com>
2020-01-23 03:22:48 -0600

    added loop

--- src/test309/testing309.java
+++ src/test309/testing309.java
@@ -2,6 +2,9 @@ package test309;
 
 public class testing309 {
 	public static void main(String [] args) {
+		for (int i=0; i<10; i++){
+			System.out.println("Line: "+i);
+		}
 		System.out.println("Hello World!");
 	}
 
